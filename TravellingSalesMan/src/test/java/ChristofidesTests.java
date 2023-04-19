bcd5bc87d8e4a989223f630f3c6504f6855556b6
Ramya Devie Saravana Bhava <saravanabhava.r@northeastern.edu>
2023-04-18 21:05:39 -0400

    add tests for christofides

--- TravellingSalesMan/src/test/java/ChristofidesTests.java
+++ TravellingSalesMan/src/test/java/ChristofidesTests.java
@@ -35,4 +35,52 @@ public class ChristofidesTests {
         int result = TSP.minKey(key, mstSet);
         Assert.assertEquals(result, expected);
     }
+
+    @Test
+    public void testFindOddVertexes(){
+        Location[] locations = TSP.readLocations("./crimeTest.csv");
+        List<int[]> MST = new ArrayList<>();
+        MST.add(new int[]{0, 5});
+        MST.add(new int[]{0, 1});
+        MST.add(new int[]{1, 4});
+        MST.add(new int[]{1, 6});
+        MST.add(new int[]{2, 6});
+        MST.add(new int[]{2, 3});
+        ArrayList expectedVertexes = new ArrayList<>();
+        expectedVertexes.add(1);
+        expectedVertexes.add(3);
+        expectedVertexes.add(4);
+        expectedVertexes.add(5);
+
+        assertArrayEquals(expectedVertexes.toArray(), TSP.findOddVertexes(MST).toArray());
+    }
+
+    @Test
+    public void testFindEulerianTour() {
+        Location[] locations = TSP.readLocations("./crimeTest.csv");
+        double[][] distanceMatrix = TSP.calculateDistanceMatrix(locations);
+        List<int[]> MST = new ArrayList<>();
+        MST.add(new int[]{0, 5});
+        MST.add(new int[]{0, 1});
+        MST.add(new int[]{1, 4});
+        MST.add(new int[]{1, 6});
+        MST.add(new int[]{2, 6});
+        MST.add(new int[]{2, 3});
+        MST.add(new int[]{4, 1, 5648});
+        MST.add(new int[]{3, 5, 29789});
+        ArrayList expectedPath = new ArrayList<>();
+        expectedPath.add(5);
+        expectedPath.add(3);
+        expectedPath.add(2);
+        expectedPath.add(6);
+        expectedPath.add(1);
+        expectedPath.add(4);
+        expectedPath.add(4);
+        expectedPath.add(0);
+        expectedPath.add(0);
+        List<Integer> tour = TSP.findEulerianTour(MST,distanceMatrix);
+
+        assertArrayEquals(expectedPath.toArray(), tour.toArray());
+    }
+
 }
